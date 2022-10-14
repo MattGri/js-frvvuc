e176dc48cb1de682fd08e8f853864f249c801156
Mateusz <mateusz.grzybowski98@gmail.com>
2022-10-14 15:47:10 +0300

    Finished code review

--- index.js
+++ index.js
@@ -1,117 +1,227 @@
-import './style.css';
-import { apiurl } from './src/constants';
+const apiurl = 'https://rickandmortyapi.com/api/character';
+const characters = document.querySelector('.charactersList');
 
-document.title = 'prework';
-
-const characters = document.querySelector('section');
-
-async function getCharacters() {
+const getCharacters = async () => {
   const response = await fetch(apiurl, {
     method: 'GET',
   });
 
-  var loading = false;
-
   return await response.json();
-}
+};
 
-(async () => {
-  var loading = true;
+const text = async () => {
   let { results, info } = await getCharacters();
 
-  document.querySelector('.search > input').value = 1;
-  document.querySelector('.search > span').innerText = info.pages;
+  document.querySelector('.inputSearch').value = 1;
+  document.querySelector('.pages').innerText = info.pages;
 
-  document.querySelector('.search > input').addEventListener('change', () => {
-    fetch(apiurl + '?page=' + this.value)
-      .then(function (res) {
+  document.querySelector('.inputSearch').addEventListener('change', () => {
+    fetch(apiurl + '?page=' + document.querySelector('.inputSearch').value)
+      .then((res) => {
         return res.json();
       })
       .then((res) => {
-        console.log(res);
         results = res.results;
       });
   });
 
-  function getOnlyAlives() {
+  const showAllCharacters = () => {
     characters.innerHTML = '';
 
     results.forEach((res, index) => {
-      if (res.status === 'Alive') {
-        results.splice(index, 1);
-      }
-    });
+      const characterCard = document.createElement('div');
+      const characterName = document.createElement('p');
+      const characterIndex = document.createElement('p');
 
-    //todo render filtered results!
-  }
+      characterName.innerText = res.name;
+      characterIndex.innerText = index + 1;
 
-  function deadsOnly() {
-    characters.innerHTML = '';
+      characterCard.appendChild(characterName);
+      characterCard.appendChild(characterIndex);
+      characters.appendChild(characterCard);
 
-    results.forEach((res, index) => {
-      if (res.status === 'Dead') {
-        results.splice(index, 1);
-      }
-    });
+      characterName.addEventListener('click', () => {
+        const n = document.createElement('p');
+        const gender = document.createElement('p');
+        const status = document.createElement('p');
+        const jpg = document.createElement('img');
+        const deleteCharacter = document.createElement('button');
+        jpg.width = '100';
 
-    //todo render filtered results!
-  }
+        n.innerText = 'imie: ' + results[index].name;
+        gender.innerText = 'płeć: ' + results[index].gender;
+        status.innerText = 'status: ' + results[index].status;
+        jpg.src = results[index].image;
+        deleteCharacter.innerText = 'usuń';
 
-  function all2() {
-    // todo
-  }
+        const details = document.querySelector('.displayedCharacters');
 
-  alive.addEventListener('click', getOnlyAlives);
-  dead.addEventListener('click', deadsOnly);
-  all.addEventListener('click', all2);
+        details.append(n, gender, status, jpg, deleteCharacter);
 
-  // adding results to DOM!
-  for (let index in results) {
-    const p = document.createElement('p');
-    const lp = document.createElement('span');
-
-    lp.innerText = index + 1;
-    const text = document.createTextNode(' ' + results[index].name);
-    p.prepend(lp, text);
-
-    characters.append(p);
-
-    p.addEventListener('click', () => {
-      results[index];
-      const n = document.createElement('div');
-      const gender = document.createElement('div');
-      const status = document.createElement('div');
-      const jpg = document.createElement('img');
-      jpg.width = '100';
-
-      n.innerText = 'imie: ' + results[index].name;
-      gender.innerText = 'płeć: ' + results[index].gender;
-      status.innerText = 'status: ' + results[index].status;
-      jpg.src = results[index].image;
-
-      const details = document.querySelector('section:not(:first-child)');
-
-      details.append(n, gender, status, jpg);
-
-      jpg.onclick = () => {
-        const dialog = document.createElement('dialog');
-        document.body.append(dialog);
-
-        dialog.append(jpg);
-        jpg.width = 300;
-
-        dialog.showModal();
-
-        const close = document.createElement('button');
-
-        close.innerText = 'zamknij';
-
-        close.addEventListener('click', () => {
-          dialog.close();
+        deleteCharacter.addEventListener('click', () => {
+          n.remove();
+          gender.remove();
+          status.remove();
+          jpg.remove();
+          deleteCharacter.remove();
         });
 
-        dialog.append(close);
-      };
+        jpg.onclick = () => {
+          const dialog = document.createElement('dialog');
+          document.body.append(dialog);
+
+          dialog.append(jpg);
+          jpg.width = 300;
+
+          dialog.showModal();
+
+          const close = document.createElement('button');
+
+          close.innerText = 'zamknij';
+
+          close.addEventListener('click', () => {
+            dialog.close();
+          });
+
+          dialog.append(close);
+        };
+      });
     });
-  }
-})();
+  };
+
+  const showOnlyAlives = () => {
+    characters.innerHTML = '';
+    const alive = results.filter((character) => character.status === 'Alive');
+
+    alive.forEach((character, index) => {
+      const characterCard = document.createElement('div');
+      const characterName = document.createElement('p');
+      const characterIndex = document.createElement('p');
+
+      characterName.innerText = character.name;
+      characterIndex.innerText = index + 1;
+
+      characters.appendChild(characterCard);
+      characterCard.appendChild(characterIndex);
+      characterCard.appendChild(characterName);
+
+      characterName.addEventListener('click', () => {
+        const n = document.createElement('p');
+        const gender = document.createElement('p');
+        const status = document.createElement('p');
+        const jpg = document.createElement('img');
+        const deleteCharacter = document.createElement('button');
+        jpg.width = '100';
+
+        n.innerText = 'imie: ' + alive[index].name;
+        gender.innerText = 'płeć: ' + alive[index].gender;
+        status.innerText = 'status: ' + alive[index].status;
+        jpg.src = alive[index].image;
+        deleteCharacter.innerText = 'usuń';
+
+        const details = document.querySelector('.displayedCharacters');
+
+        details.append(n, gender, status, jpg, deleteCharacter);
+
+        deleteCharacter.addEventListener('click', () => {
+          n.remove();
+          gender.remove();
+          status.remove();
+          jpg.remove();
+          deleteCharacter.remove();
+        });
+
+        jpg.onclick = () => {
+          const dialog = document.createElement('dialog');
+          document.body.append(dialog);
+
+          dialog.append(jpg);
+          jpg.width = 300;
+
+          dialog.showModal();
+
+          const close = document.createElement('button');
+
+          close.innerText = 'zamknij';
+
+          close.addEventListener('click', () => {
+            dialog.close();
+          });
+
+          dialog.append(close);
+        };
+      });
+    });
+  };
+
+  const showDeadsOnly = () => {
+    characters.innerHTML = '';
+    const dead = results.filter((character) => character.status === 'Dead');
+
+    dead.forEach((character, index) => {
+      const characterCard = document.createElement('div');
+      const characterName = document.createElement('p');
+      const characterIndex = document.createElement('p');
+
+      characterName.innerText = character.name;
+      characterIndex.innerText = index + 1;
+
+      characterCard.appendChild(characterName);
+      characterCard.appendChild(characterIndex);
+      characters.appendChild(characterCard);
+
+      characterName.addEventListener('click', () => {
+        const n = document.createElement('p');
+        const gender = document.createElement('p');
+        const status = document.createElement('p');
+        const jpg = document.createElement('img');
+        const deleteCharacter = document.createElement('button');
+        jpg.width = '100';
+
+        n.innerText = 'imie: ' + dead[index].name;
+        gender.innerText = 'płeć: ' + dead[index].gender;
+        status.innerText = 'status: ' + dead[index].status;
+        jpg.src = dead[index].image;
+        deleteCharacter.innerText = 'usuń';
+
+        const details = document.querySelector('.displayedCharacters');
+
+        details.append(n, gender, status, jpg, deleteCharacter);
+
+        deleteCharacter.addEventListener('click', () => {
+          n.remove();
+          gender.remove();
+          status.remove();
+          jpg.remove();
+          deleteCharacter.remove();
+        });
+
+        jpg.onclick = () => {
+          const dialog = document.createElement('dialog');
+          document.body.append(dialog);
+
+          dialog.append(jpg);
+          jpg.width = 300;
+
+          dialog.showModal();
+
+          const close = document.createElement('button');
+
+          close.innerText = 'zamknij';
+
+          close.addEventListener('click', () => {
+            dialog.close();
+          });
+
+          dialog.append(close);
+        };
+      });
+    });
+  };
+
+  all.addEventListener('click', showAllCharacters);
+  alive.addEventListener('click', showOnlyAlives);
+  dead.addEventListener('click', showDeadsOnly);
+};
+
+text();
